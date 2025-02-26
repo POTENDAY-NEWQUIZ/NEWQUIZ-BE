d00aa54fce8b443b281997043fc6729acdefefe0
oosedus <seayeon0729@gmail.com>
2025-02-26 16:35:46 +0900

    [feat] : 퀴즈 조회 DTO 생성

---
+++ src/main/java/com/example/newquiz/dto/response/QuizResponse.java
@@ -0,0 +1,67 @@
+package com.example.newquiz.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+public class QuizResponse {
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class QuizListDto{
+        private int quizCount;
+        private List<SynonymQuizDto> synonymQuiz;
+        private List<MeaningQuizDto> meaningQuiz;
+        private List<ContentQuizDto> contentQuiz;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class SynonymQuizDto {
+        private String category;
+        private Long quizId;
+        private Long paragraphId;
+        private String sourceSentence;
+        private String word;
+        private String option1;
+        private String option2;
+        private String option3;
+        private String option4;
+        private int answer;
+        private String explanation;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class MeaningQuizDto {
+        private String category;
+        private Long quizId;
+        private Long paragraphId;
+        private String sourceSentence;
+        private String word;
+        private String option1;
+        private String option2;
+        private String option3;
+        private String option4;
+        private int answer;
+        private String explanation;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class ContentQuizDto {
+        private String category;
+        private Long quizId;
+        private Long paragraphId;
+        private String question;
+        private String answer;
+        private String explanation;
+
+    }
+}
